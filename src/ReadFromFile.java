26e375e050321178214d6e00e41382f42acf416c
alexandr-osipov <alexandr.osipov@idfinance.com>
2025-02-07 18:48:34 +0300

    AlishevLessons

--- src/ReadFromFile.java
+++ src/ReadFromFile.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ReadFromFile {
     public static void main(String[] args) throws FileNotFoundException {
 
-        String path = "c:\\Users\\alexandr.osipov\\Documents\\Tech Help\\Hot keys.txt";
+        String path = "\\Users\\alexandr.osipov\\Documents\\Tech Help\\Hot keys.txt";
 
         File file = new File(path);
         Scanner sc = new Scanner(file);
