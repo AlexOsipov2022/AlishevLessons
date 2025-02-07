6a1ff7d8431a35bd5d2afd94378c9ac6baac4990
alexandr-osipov <alexandr.osipov@idfinance.com>
2025-02-07 18:48:08 +0300

    AlishevLessons

---
+++ src/ReadFromFile.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFromFile {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String path = "c:\\Users\\alexandr.osipov\\Documents\\Tech Help\\Hot keys.txt";
+
+        File file = new File(path);
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
